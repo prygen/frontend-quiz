7145aec9be3d9ede6a448a1e1bec337c2be39917
prygen <1999.batin.ilya@mail.ru>
2023-04-11 12:43:11 +0400

    frontend-quiz
    frontend-quiz react

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+import "./index.scss";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} из {questions.length} ответов
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round(step / questions.length) * 100;
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress_inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants?.map((text, index) => {
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>;
+        })}
+      </ul>
+    </>
+  );
+}
+
+function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+  return (
+    <div className="App">
+      {step !== questions.length ? (
+        <Game
+          step={step}
+          question={questions}
+          onClickVariant={onClickVariant}
+        />
+      ) : (
+        <Result correct={correct} />
+      )}
+    </div>
+  );
+}
+
+export default App;
